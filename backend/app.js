2f50859ebe09e8113ad934d3a8b3e84931393c84
SharadJhanwar <sharadjhawar12345@gmail.com>
2025-10-05 01:15:30 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,33 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const PORT = 5001;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors")
+
+const toDoItemsRouter = require("./routes/toDoItemsRouter")
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  console.log("MONGODB_URI is not present in .env");
+}
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log("HEELO USER ");
-  next()
+  next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the homepage!');
-});
+app.use("/api/todo",toDoItemsRouter
+)
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log("CONNECTED TO DB")
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
